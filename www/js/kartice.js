e9e84edde4992663de6b683db19adf3d6a5d92f1
Marina Tunjic <Tunjic.Marina@feg.eu>
2021-06-02 13:02:31 +0200

    change

--- www/js/kartice.js
+++ www/js/kartice.js
@@ -21,86 +21,7 @@ odgovor=JSON.parse(ioo);
 temp=odgovor;
 prikaziMacice(temp);
 
-function prikaziMacice(param){
-  document.getElementById("album-rotator").innerHTML="";
-  for( var i = 0; i < 20; i++){ // 
-    var albumdetail=document.createElement("span");
-    albumdetail.setAttribute("class","album-details");
-  
-    var icon=document.createElement("span");
-    icon.setAttribute("class","icon");
-    icon.style.background="url(" +param[i].Image+") center / 260px 230px no-repeat";
-    var subtex=document.createElement("span");
-    subtex.setAttribute("class","subtext");
-    subtex.innerHTML="Ime: "+ param[i].name +"<br>Dob: "+param[i].starost+"m"+"<br>Boja: "+param[i].boja;
-    var btn=document.createElement("button");
-    btn.setAttribute("class","udomi");
-    btn.setAttribute("value",i);
-    btn.textContent="Udomi";
-    albumdetail.appendChild(icon);
-    albumdetail.appendChild(subtex);
-    albumdetail.appendChild(btn);
-  
-    var albumitem=document.createElement("div");
-    albumitem.setAttribute("class","album-item");
-    albumitem.appendChild(albumdetail);
-  
-    document.getElementById("album-rotator").appendChild(albumitem);
-    
-    }
-    
-    
-      param=param.sort((firstItem, secondItem) => firstItem.starost - secondItem.starost);
-  
-      var ucitaj=document.createElement("button");
-      ucitaj.setAttribute("class","prikaziVise");
-      ucitaj.setAttribute("id","ucitaj");
-      ucitaj.textContent="Prikaži više";
-      document.getElementById("album-rotator").appendChild(ucitaj);
-  
-      ucitaj.onclick = function(){
-        document.getElementById("ucitaj").style.display = "none";
-        document.getElementById("album-rotator").innerHTML="";
 
-        for( var i = 0; i < param.length; i++){ 
-          var albumdetail=document.createElement("span");
-          albumdetail.setAttribute("class","album-details");
-        
-          var icon=document.createElement("span");
-          icon.setAttribute("class","icon");
-          icon.style.background="url(" +param[i].Image+") center / 260px 230px no-repeat";
-          var subtex=document.createElement("span");
-          subtex.setAttribute("class","subtext");
-          subtex.innerHTML="Ime: "+ param[i].name +"<br>Dob: "+param[i].starost+"m"+"<br>Boja: "+param[i].boja;
-          var btn=document.createElement("button");
-          btn.setAttribute("value",i);
-          btn.setAttribute("class","udomi");
-          btn.textContent="Udomi";
-          albumdetail.appendChild(icon);
-          albumdetail.appendChild(subtex);
-          albumdetail.appendChild(btn);
-        
-          var albumitem=document.createElement("div");
-          albumitem.setAttribute("class","album-item");
-          albumitem.appendChild(albumdetail);
-        
-          document.getElementById("album-rotator").appendChild(albumitem);
-          
-        }
-        console.log("dsafsdf");
-  temp=param;
-  var btns=document.querySelectorAll(".udomi");
-  btns.forEach(btn=>btn.addEventListener('click',function(){_udomi(btn)}));
-  
-      }
-      //temp=odgovor; 
-      
-  console.log("dsafsdf");
-  temp=param;
-  var btns=document.querySelectorAll(".udomi");
-  btns.forEach(btn=>btn.addEventListener('click',function(){_udomi(btn)}));
-  
-  }
 
   
 
@@ -124,10 +45,10 @@ function _udomi(btn){
 
   var checks=document.querySelectorAll('input[type=checkbox][name="dodatnifilter"]');
   checks.forEach(check => check.addEventListener('change',()=>myfunction()));
-  var radios = document.querySelectorAll('input[type=radio][name="sort"]');
-  var radioss = document.querySelectorAll('input[type=radio][name="smjer"]');
-  radios.forEach(radio => radio.addEventListener('change', () => myfunc()));
-  radioss.forEach(radio => radio.addEventListener('change', () => myfunc()));
+  var radio1 = document.querySelectorAll('input[type=radio][name="sort"]');
+  var radio2 = document.querySelectorAll('input[type=radio][name="smjer"]');
+  radio1.forEach(radio => radio.addEventListener('change', () => myfunc()));
+  radio2.forEach(radio => radio.addEventListener('change', () => myfunc()));
   
 
   function myfunction(){
@@ -197,34 +118,3 @@ function pretrazi() {
   
   
 }
-function prikaziMacice1(param){
-  document.getElementById("album-rotator").innerHTML="";
-  for( var i = 0; i < param.length; i++){ // 
-    var albumdetail=document.createElement("span");
-    albumdetail.setAttribute("class","album-details");
-  
-    var icon=document.createElement("span");
-    icon.setAttribute("class","icon");
-    icon.style.background="url(" +param[i].Image+") center / 260px 230px no-repeat";
-    var subtex=document.createElement("span");
-    subtex.setAttribute("class","subtext");
-    subtex.innerHTML="Ime: "+ param[i].name +"<br>Dob: "+param[i].starost+"m"+"<br>Boja: "+param[i].boja;
-    var btn=document.createElement("button");
-    btn.setAttribute("class","udomi");
-    btn.setAttribute("value",i);
-    btn.textContent="Udomi";
-    albumdetail.appendChild(icon);
-    albumdetail.appendChild(subtex);
-    albumdetail.appendChild(btn);
-  
-    var albumitem=document.createElement("div");
-    albumitem.setAttribute("class","album-item");
-    albumitem.appendChild(albumdetail);
-  
-    document.getElementById("album-rotator").appendChild(albumitem);
-    
-    }
-    temp=param;
-  var btns=document.querySelectorAll(".udomi");
-  btns.forEach(btn=>btn.addEventListener('click',function(){_udomi(btn)}));
-  }
